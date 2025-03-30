50546c3b341d92ab0820372e85d4d88c61a20c6d
vikram kumar <vikramkumar8t8@gmail.com>
2025-03-30 22:59:16 +0500

    update

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
      return (
-          <div className='max-w-[1360px] w-full mx-auto mt-[35px] bg-white rounded-[45px] h-[622px] px-6 py-3'>Hero</div>
+          <div className='px-5'>
+               <div className='max-w-[1360px] w-full mx-auto mt-[35px] bg-white rounded-[45px] h-[622px] px-6 py-3'>Hero</div>
+          </div>
      )
 }
 
