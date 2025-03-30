b87eb69472a7b308386b51ee3e71f202ee205728
vikram kumar <vikramkumar8t8@gmail.com>
2025-03-30 23:18:11 +0500

    update

--- app/components/NewArrivals.js
+++ app/components/NewArrivals.js
@@ -1,6 +1,6 @@
 "use client"
 import { Swiper, SwiperSlide } from "swiper/react"
-import {Autoplay } from "swiper/modules"
+import { Autoplay } from "swiper/modules"
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/mousewheel"
@@ -88,16 +88,16 @@ const NewArrivals = () => {
      return (
           <div className="mt-[60px] w-full pl-5 md:pl-0">
                <div className="max-w-[1296px] mx-auto">
-                    <h2 className="text-[24px] md:text-4xl font-semibold leading-[34px] mb-6 md:mb-9 sm:text-left text-center">New Arrivals</h2>
+                    <h2 className="text-[24px] md:text-4xl font-semibold leading-[34px] mb-6 md:mb-9">New Arrivals</h2>
                </div>
 
-               <div className="w-full overflow-hidden">
+               <div className="w-full overflow-hidden sm:block hidden">
                     <Swiper
                          spaceBetween={20}
                          slidesPerView="auto"
                          modules={[Autoplay]}
                          className="mySwiper"
-                         // wrapperClass="!flex"
+                    // wrapperClass="!flex"
                     >
                          {newArrivalsData.map((watch) => (
                               <SwiperSlide key={watch.id} className="w-[207px] max-w-[207px] sm:w-[284px] sm:max-w-[284px] flex-shrink-0">
@@ -154,6 +154,63 @@ const NewArrivals = () => {
                               </SwiperSlide>
                          ))}
                     </Swiper>
+               </div>
+
+               <div className="grid grid-cols-2 gap-3">
+                    {newArrivalsData.map((watch) => (
+                         <SwiperSlide key={watch.id} className="">
+                              <div className="bg-white rounded-[20px] sm:rounded-[30px] p-3 sm:p-[18px] h-full">
+                                   <div className="w-full relative">
+                                        <Image
+                                             src={watch.imageUrl || "/placeholder.svg"}
+                                             alt={`${watch.brand} ${watch.model}`}
+                                             width={248}
+                                             height={248}
+                                             draggable="false"
+                                             className="w-full sm:w-[248px] sm:h-[248px]"
+                                        />
+                                        {watch.discount > 0 && (
+                                             <div className="bg-[#60FF7D] rounded-lg sm:rounded-[13px] p-2 sm:p-2.5 absolute w-fit top-0 left-0">
+                                                  <h2 className="text-black text-xs sm:text-sm font-normal">-{watch.discount}%</h2>
+                                             </div>
+                                        )}
+                                   </div>
+                                   <div className="mt-3 sm:mt-[12px] space-y-3">
+                                        <h3 className="text-black font-normal !leading-[12px] text-sm">{watch.brand}</h3>
+                                        <h2 className="text-black font-semibold !leading-[17px] text-[18px]">{watch.model}</h2>
+                                        <p className="text-black text-sm !leading-[12px]font-normal">{watch.reference}</p>
+                                   </div>
+                                   <div className="mt-[12px] w-full flex items-center justify-start">
+                                        <div className="w-fit bg-[#ECF0F3] p-[9px] rounded-xl">
+                                             <h3 className="text-[#000000] text-xs sm:text-[14px] font-normal !leading-[10px]">Year {watch.year}</h3>
+                                             <div className="mt-3 flex items-center gap-2 sm:gap-3">
+                                                  <h3 className="flex items-center gap-1 sm:gap-2 text-black text-xs !leading-[10px]">
+                                                       Papers {watch.condition.includes("Papers") ? <Right /> : <Cross />}
+                                                  </h3>
+                                                  <h3 className="flex items-center gap-1 sm:gap-2 text-black text-xs !leading-[10px]">
+                                                       Box {watch.condition.includes("Box") ? <Right /> : <Cross />}
+                                                  </h3>
+                                             </div>
+                                        </div>
+                                   </div>
+                                   <div className="my-[12px] flex items-center justify-start gap-2 sm:gap-[18px]">
+                                        <h3 className={`text-[18px] !leading-[29px] font-semibold ${watch.discount > 0 ? "text-[#017EFE]" : "text-black"}`}>
+                                             {watch.price}$
+                                        </h3>
+                                        {watch.discount > 0 && (
+                                             <del className="text-[#828282] text-sm leading-none font-normal">{watch.originalPrice}$</del>
+                                        )}
+                                   </div>
+                                   <div className="flex items-center justify-start">
+                                        <button className="bg-[#017EFE] hover:bg-[#003D7B] transition-all duration-300 h-[39px] rounded-[60px] flex items-center justify-center px-10 text-white font-medium text-[16px] leading-none w-fit">
+                                             <h3 className="sm:block hidden">Contact</h3>
+                                             <h3 className="sm:hidden block">Buy</h3>
+                                        </button>
+                                   </div>
+
+                              </div>
+                         </SwiperSlide>
+                    ))}
                </div>
           </div>
      )
