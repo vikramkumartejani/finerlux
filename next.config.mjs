8d905e274e1b67399632f69a9fd359213486e9be
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-04 03:47:25 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+     images: {
+       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+       formats: ['image/webp', 'image/avif'],
+     },
+     compress: true,
+     webpack(config) {
+       config.module.rules.push({
+         test: /\.svg$/,
+         use: ['@svgr/webpack']
+       });
+       return config;
+     }
+   }
+   
+   export default nextConfig;
