57126318713c1ffced3f31f3b639912bcd27a7db
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-06 09:05:45 +0500

    update

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -131,15 +131,22 @@ const Footer = () => {
                 </li>
                 <li
                   className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px] cursor-pointer hover:text-[#017EFE] transition-colors"
-                  onClick={() => scrollToHomeFormSection("part-exchange")}
+                  onClick={() => scrollToHomeFormSection("sell")}
                 >
-                  {t("footer.services.partExchange")}
+                  {t("footer.services.sell")}
                 </li>
                 <li
                   className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px] cursor-pointer hover:text-[#017EFE] transition-colors"
-                  onClick={() => scrollToHomeFormSection("authenticate")}
+                  onClick={() => scrollToHomeFormSection("buy")}
                 >
-                  {t("footer.services.authenticate")}
+                  {t("footer.services.buy")}
+                </li>
+
+                <li
+                  className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px] cursor-pointer hover:text-[#017EFE] transition-colors"
+                  onClick={() => scrollToHomeFormSection("part-exchange")}
+                >
+                  {t("footer.services.partExchange")}
                 </li>
                 <li
                   className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px] cursor-pointer hover:text-[#017EFE] transition-colors"
@@ -147,8 +154,11 @@ const Footer = () => {
                 >
                   {t("footer.services.source")}
                 </li>
-                <li className="text-black text-[14px] md:text-[18px] font-normal !leading-[22px]">
-                  <BuySellLinks text={t("footer.services.buySell")} />
+                <li
+                  className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px] cursor-pointer hover:text-[#017EFE] transition-colors"
+                  onClick={() => scrollToHomeFormSection("authenticate")}
+                >
+                  {t("footer.services.authenticate")}
                 </li>
               </ul>
 
