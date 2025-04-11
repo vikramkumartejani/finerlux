6bced5098b53cc23bc26d8851f77584c869ca3b9
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-11 17:27:53 +0500

    update

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -136,14 +136,14 @@ const Hero = () => {
       {/* Mobile View */}
       <div className="md:hidden block">
         <div className="h-[265px] w-full bg-white rounded-[30px] relative flex items-end justify-end">
-            <Image
+            {/* <Image
               src="/assets/mobile.svg"
               alt="mobile"
               className="w-full h-full"
               priority
               width={200}
               height={265}
-            />
+            /> */}
           <div className="absolute z-40 flex items-end justify-end right-2 top-0 h-full w-fit">
             <Image
               src="/assets/hero-watch.webp"
