3bec5087d98568979aa455465ffc901187233721
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-11 17:20:12 +0500

    update

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -138,9 +138,6 @@ const Hero = () => {
         <div className="h-[265px] w-full bg-white rounded-[30px] relative flex items-end justify-end">
             <Image
               src="/assets/mobile.svg"
-              fetchPriority="high"
-              decoding="async"
-              style={{color:"transparent"}}
               alt="mobile"
               className="w-full h-full"
               priority
