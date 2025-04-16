633b1f48696815bf4ac503d3d67c1a3e9f772506
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-17 03:55:02 +0500

    Meta Tag Added

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -78,10 +78,10 @@ const Footer = () => {
                   Hatton Garden, London
                 </li>
                 <li className="text-black text-[14px] md:text-[18px] font-normal mb-[12px] lg:mb-[18px] !leading-[22px]">
-                  info@finerlux.com
+                  <a href="mailto:info@finerlux.com">info@finerlux.com</a>
                 </li>
                 <li className="text-black text-[14px] md:text-[18px] font-normal !leading-[22px]">
-                  +44 123 456 7890
+                  <a href="tel:+441234567890">+44 123 456 7890</a>
                 </li>
               </ul>
 
