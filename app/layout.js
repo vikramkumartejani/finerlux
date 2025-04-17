d7757296f617055aaf4d84e5530dd80b3f2fda4e
vikram kumar <vikramkumar8t8@gmail.com>
2025-04-18 02:17:22 +0500

    update

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,20 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qtx9p399lq");
+            `
+          }}
+        />
+      </head>
       <body>
         <Navbar />
         {children}
